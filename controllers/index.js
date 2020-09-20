a79bfe40e5986fb3a41ff66440df41620a9b9965
Admin <manuelperdigonsocas@gmail.com>
2020-09-20 22:23:55 +0200

    Tests, set Timeout and last details

--- controllers/index.js
+++ controllers/index.js
@@ -1,59 +1,66 @@
 const flightsAPI = require('./getFlightsData');
 const { json } = require('express');
 
-const allFlights = [];
-
-exports.getFlights = async () => {
-    const source1 = await flightsAPI.getFlightsAPI1(process.env.LINK_ONE);
-    const source2 = await flightsAPI.getFlightsAPI1(process.env.LINK_TWO);
-    const flightsPromise = new Promise((resolve, reject) => {
-        if (source1.data != undefined || source2.data != undefined) {
-            resolve(allFlights.push(source1.data.flights, source2.data.flights));
+const isSameSlice = (slice1, slice2) => {
+    for (let i = 0; i < slice1.length; i++) {
+        let isFound = false;
+        for (let j = 0; j < slice2.length; j++) {
+            if (slice1.flight_number == slice2.flight_number && slice1.departure_date_time_utc == slice2.departure_date_time_utc) {
+                isFound = true;
+                break
+            }
         }
-        else {
-            reject('Unable to get flights info, please try again')
+        if (isFound == false) {
+            return false
         }
-    });
-    return allFlights;
-}
+    }
+    return true
+};
 
-exports.deleteDuplicates = async () => {
-    const duplicatedFilter = new Map();
-    const filter= [];
-    const flightsTogether = [...allFlights[0], ...allFlights[1]];
-    const loop = flightsTogether.forEach((val, i) => {
-        allSlices.push(val.slices);
-        // if (!duplicatedFilter.has(val.slices[i].flight_number)) { 
-        //     duplicatedFilter.set(val.slices[i].flight_number, val.slices[i].departure_date_time_utc);
-        // }
-    })
-    // for (const [key, val] of allArray) {
-    //     const { flight_number, departure_date_time_utc } = val[1].slices;
-    //     if (!duplicatedFilter.has(flight_number)) {
-    //         duplicatedFilter.set(flight_number, departure_date_time_utc);
-    //     }
-    // //     else {
-    // //         // const index = allFlights.data.flights.slices.indexOf(i);
-    // //         // allFlights.data.flights.slices.slice(index); 
-    // //     }
-    // }
-    console.log(allSlices);
-    return duplicatedFilter;
+const isSameFlight = (flight1, flight2) => {
+    if (flight1.slices.length != flight2.slices.length) {
+        return false;
+    }
+    if (flight1.price != flight2.price) {
+        return false;
+    }
+    if (isSameSlice(flight1.slices, flight2.slices) == false) {
+        return false
+    }
+    return true
+};
+
+exports.mergeFlightLists = async (source1, source2) => {
+    const flightsToAdd = [];
+    for (let i = 0; i < source1.length; i++) {
+        let isFound = false;
+        for (let j = 0; j < source2.length; j++) {
+            if (isSameFlight(source1[i], source2[j]) == true) {
+                isFound = true;
+                break
+            }
+        }
+        if (isFound == false) {
+            flightsToAdd.push(source1[i]);
+        }
+    }
+    return [...source2, ...flightsToAdd];
 }
 
 exports.response = async (req, res, next) => {
     try {
-        const getRequest = await this.getFlights();
-        const deleteDuplicates = await this.deleteDuplicates();
+        const source1 = await flightsAPI.getFlights(process.env.LINK_ONE);
+        const source2 = await flightsAPI.getFlights(process.env.LINK_TWO);
+        const flights = await this.mergeFlightLists(source1, source2);
         res.status(200).json({
             status: 'success',
-            data: JSON.stringify(deleteDuplicates)
+            data: JSON.stringify(flights)
         })
-
     } catch (err) {
+        console.log(err.message);
         res.status(400).json({
             status: 'failed',
-            message: 'An error has happened getting all flights, please try again'
+            message: 'Unable to get requested flights, please try again'
         })
     }
 };
--- test/test.js
+++ test/test.js
@@ -1,3 +1,14 @@
+const dotenv = require('dotenv').config({ path: `${__dirname}/../config.env` });
 const chai = require('chai');
+const { assert } = require('chai');
+const flightController = require('../controllers/index');
+const flightsAPI = require('../controllers/getFlightsData');
 
-describe()
+describe('controllers/index.js', function () {
+
+    it('should remove duplicated flights', async function () {
+        const source1 = await flightsAPI.getFlights(process.env.LINK_ONE);
+        const source2 = await flightsAPI.getFlights(process.env.LINK_TWO);
+        assert.isArray(await flightController.mergeFlightLists(source1, source2), 'Create final flights list correctly');
+    });
+});
